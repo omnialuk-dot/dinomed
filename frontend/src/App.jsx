65fb21a5f0965f6b02015e181e630871fd2d2878
omnialuk-dot <omnialuk@gmail.com>
2026-01-31 17:06:23 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import SiteHeader from "./components/SiteHeader";
 
 import Home from "./pages/Home";
 import Dispense from "./pages/Dispense";
-import SimulazioniRun from "./pages/SimulazioniRun";
 import ChiSiamo from "./pages/ChiSiamo";
 import Contatti from "./pages/Contatti";
 import SimulazioniRun from "./pages/SimulazioniRun.jsx";
