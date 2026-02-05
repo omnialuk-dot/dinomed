7c434c2468887ba66b8712272b3e67c65de66c24
omnialuk-dot <omnialuk@gmail.com>
2026-02-06 00:02:26 +0100

    a

--- backend/sql/dispense_add_pubblicata.sql
+++
@@ -1,3 +0,0 @@
--- If your dispense table doesn't have these columns, run:
-alter table dispense add column if not exists pubblicata boolean default false;
-alter table dispense add column if not exists created_at timestamptz default now();
